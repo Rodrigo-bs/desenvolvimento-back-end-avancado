9623b4c22860c5b5615777d6d58dc839a522d0c7
unknown <rodrigo.bs.548@gmail.com>
2022-06-26 21:21:20 -0300

    Adicionei no server.js

--- project-02/server.js
+++ project-02/server.js
@@ -4,14 +4,24 @@ const path = require('path')
 
 http.createServer((req, res) => {
 
+    const file = req.url === '/' ? 'index.html' : req.url;
+    const filePath = path.join(__dirname, 'public', file);
+
+    const extName = path.extname(filePath);
+    const allowedExtensions = ['.html', '.js', '.css'];
+
+    const allowed = allowedExtensions.find(item => item == extName);
+
+    if (!allowed) return
+
     if (req.url === '/')
         fs.readFile(
-            path.join(__dirname, 'public', 'index.html'),
+            filePath,
             (err, content) => {
                 if (err) throw err
 
                 res.end(content)
             }
         )
-
+    
 }).listen(5000, () => { console.log('Server is running.') })
